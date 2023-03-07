dc4a4a90e85551284ed4bc40413a0de29f94c46d
umazing-zer0 <djagpdud125@korea.ac.kr>
2023-03-07 23:57:21 +0900

    230307 update

--- Baekjoon/10869. the four arithmetic.py
+++ Baekjoon/10869. the four arithmetic.py
@@ -6,3 +6,11 @@ print(num1-num2)
 print(num1*num2)
 print(num1//num2)
 print(num1%num2)
+
+# 오늘 배운 내용 중 기록할 것
+# 1) input은 입력값을 문자열로 받음
+# 2) print(int(input())) 처럼 입력값을 바로 출력도 가능
+
+# 느낀점
+# 1) 백준 풀기 시작! 파이팅
+# 2) 욕심내지 말고 하나씩, 꾸준함의 무서움을 보여주자
