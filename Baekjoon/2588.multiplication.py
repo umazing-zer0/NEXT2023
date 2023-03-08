e2c0c5696ac9dad2b2ff501f2530270c75eb3ec9
umazing-zer0 <djagpdud125@korea.ac.kr>
2023-03-08 23:54:50 +0900

    230308 백준

---
+++ Baekjoon/2588.multiplication.py
@@ -0,0 +1,12 @@
+num1=input()
+num2=input()
+
+a=int(num1)*int(num2[2])
+b=int(num1)*int(num2[1])
+c=int(num1)*int(num2[0])
+print(a)
+print(b)
+print(c)
+print(a+b*10+c*100)
+
+#파이썬은 문자열 인덱싱을 제공한다! 힌트 없이 풀 수 있다는 게 행복하다 :) 오늘도 하나 완료
